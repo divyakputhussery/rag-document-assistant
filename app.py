7f5eb8c4686962355ebf2be187c63cce3f5a00be
divyakputhussery <divyasharath86@icloud.com>
2026-04-11 21:24:34 +1000

    add pdf loading and test preview

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("RAG Document Assistant - Starting...")
+from src.loader import load_pdf
+
+def main():
+    pdf_path = "data/sample.pdf"
+
+    print("Loading PDF...")
+    documents = load_pdf(pdf_path)
+
+    print(f"Loaded {len(documents)} pages.")
+
+    # print first page content
+    print("\nFirst page preview:\n")
+    print(documents[0].page_content[:500])
+
+
+if __name__ == "__main__":
+    main()
